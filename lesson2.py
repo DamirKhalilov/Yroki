eb211acd0a2480d8906965e662dca9783aef9152
Damir Khalilov <domi.95.msuu@gmail.com>
2023-02-09 11:24:27 +0100

    first commit

---
+++ lesson2.py
@@ -0,0 +1,29 @@
+"""
+Условия
+if
+elif
+else
+
+and , и
+or , или
+not  отрицания
+# oktotorp, sharp, heshteg
+
+
+
+"""
+#print ('dddd')
+#a = input()
+#print(a)
+#a=input('damir: ')
+#print(a)
+#print('10',type('10'))
+#print(10,type(10))
+
+
+
+
+
+
+
+
